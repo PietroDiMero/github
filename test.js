743c94a890f6bd35f6af1a512f133b1c28383207
Pierre ROYER <pierreroyer@Pierrot.local>
2025-05-14 09:22:28 +0200

    Mon premier commit

---
+++ test.js
@@ -0,0 +1,9 @@
+function direBonjour() {
+    const nom = prompt("Quel est ton prénom ?");
+   if (nom) {
+   alert(`Bonjour ${nom} ! Ravi de te voir ici.`);
+ } else {
+ alert("Tu n'as rien saisi !");
+    }
+  }
+  
